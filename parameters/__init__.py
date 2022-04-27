76c546e52883510bc67ec440688f7a80ada5dfb0
Matteo Pompili <matpompili@users.noreply.github.com>
2022-04-26 23:11:45 -0500

    bump version

--- parameters/__init__.py
+++ parameters/__init__.py
@@ -2,7 +2,7 @@ from .parameters import Parameters
 from .update import update_parameters
 
 __title__ = "parameters"
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __author__ = "Matteo Pompili"
 __license__ = "MIT"
 __copyright__ = "Copyright 2022 Matteo Pompili"
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt") as installation_requirements_file:
 
 setup(
     name="parameters",
-    version="0.1.0",
+    version="0.2.0",
     packages=["parameters"],
     url="https://github.com/matpompili/parameters",
     author="Matteo Pompili",
