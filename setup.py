92b832cf189f1b7760eac244f616cc52b42a146d
Matteo Pompili <matpompili@users.noreply.github.com>
2022-04-27 08:58:27 -0500

    Add files for pypi publishing

--- setup.py
+++ setup.py
@@ -10,11 +10,7 @@ setup(
     url="https://github.com/matpompili/yamlpars",
     author="Matteo Pompili",
     classifiers=[
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Intended Audience :: Developers",
