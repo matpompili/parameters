fe58a9fdb19507fb7fc1c6225ef5e054626ec54a
Matteo Pompili <matpompili@users.noreply.github.com>
2022-09-09 09:12:09 -0500

    Bump version: 0.4.1 → 0.4.2-beta.0

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ project_urls = {
 
 setup(
     name="yamlpars",
-    version="0.4.1",
+    version="0.4.2-beta.0",
     packages=["yamlpars"],
     author="Matteo Pompili",
     python_requires=">=3.8",
